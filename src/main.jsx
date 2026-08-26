001f4e5b1e6ba14edf57fbec785a42b968838b3d
Ivan Kekar <vdeynesheko20@gmail.com>
2026-08-26 17:32:47 +0400

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { Provider } from 'react-redux'
 
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 
 import './index.css'
 import App from './routes/App.jsx'
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
 
-        <BrowserRouter>
+        <HashRouter>
         
             <Routes>
 
@@ -23,7 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
             </Routes>
         
-        </BrowserRouter>
+        </HashRouter>
       
     </Provider>
   </StrictMode>
